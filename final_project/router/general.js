1bf2854683562fca64e71783b510b64d61336bca
Madie <madeleinne.tan>
2025-11-13 18:33:15 -0500

    Working on task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,24 +12,54 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.json(books);
+  res.json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
+//   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author.toLowerCase();
+    const booksByAuthor = [];
+  
+    Object.keys(books).forEach((key) => {
+      console.log(`Checking author: ${books[key].author}`); // debug
+      if (books[key].author.toLowerCase() === author) {
+        booksByAuthor.push(books[key]);
+      }
+    });
+  
+    if (booksByAuthor.length > 0) {
+      res.json(booksByAuthor);
+    } else {
+      res.status(404).json({ message: "No books found by this author" });
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
+    const title = req.params.title.toLowerCase();
+    const booksByTitle = [];
+
+    Object.values(books).forEach((value) => {
+        console.log(`Checking title: ${books[value].title}`); // debug
+        if (books[value].title.toLowerCase() === title) {
+        booksByTitle.push(books[value]);
+        }
+    });
+
+    if (booksByTitle.length > 0) {
+        res.json(booksByTitle);
+    } else {
+        res.status(404).json({ message: "No books found by this title" });
+    }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
