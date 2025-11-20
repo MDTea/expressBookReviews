07089716174415d75ae2e17ab3ba0c4db6958e6a
Madie <madeleinne.tan@gmail.com>
2025-11-19 19:07:55 -0500

    In the middle of task 8, realizing I did tasks 6 and 7 wrong. Don't worryyyy be happy

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -70,22 +70,41 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    //You have to give a review as a request query & it must get posted with the username (stored in the session) posted. 
-    //If the same user posts a different review on the same ISBN, it should modify the existing review. 
-    //If another user logs in and posts a review on the same ISBN, it will get added as a different review under the same ISBN.
-    // Verify the web token first
+
+    // Verify the web token 
     let token = req.session.authenticated['accessToken'];
     jwt.verify(token, "access", (err, user) => {
-        if(!err){
-            req.user = user;
-            next(); // Proceed to next middleware
-        }
-        else{
+        if(err){
             return res.status(403).json({ message: "User not authenticated"});
         }
-    });
 
-    return res.status(300).json({message: "Yet to be implemented"});
+        req.user = user;
+
+        // Set up the consts
+        const thisUser = req.user.username;
+        const thisIsbn = req.body.isbn;
+        const thisReview = req.body.review;
+        
+        // validate the input
+        if(!thisIsbn || !thisReview){
+            return res.status(400).json({message : "There are fields of information missing. Please review and retry request"});
+        }
+
+        const thisBook = books[isbn]; // because isbn = index
+        if(!thisBook){
+            return res.status(404).json({message: "The book that you're reviewing about does not exist in our database"});
+        }
+
+        // If the reviews are nonexistent: set up a new map of reviews if the book doesn't have any
+        if(!thisBook.reviews){
+            thisBook.reviews = {};
+        }
+
+        // If reviews exists:
+        thisBook.reviews[thisUser] = req.body.review;
+
+        return res.status(201).json({message: "Your review has been added.", reviews: thisBook.reviews});
+    });
 });
 
 module.exports.authenticated = regd_users;
